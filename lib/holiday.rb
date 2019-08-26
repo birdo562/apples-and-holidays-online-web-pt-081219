f44bd1ae2537b5d42158b8cf030730621bce4508
Roberto Medina <robby.medina562@gmail.com>
2019-08-26 06:15:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |holiday, supply|
-  puts "#{holiday.capatalize}"
+  puts "#{holiday.capatalize}:"
   holiday.each do |holiday, decorations|
+    
 end
 end
 
