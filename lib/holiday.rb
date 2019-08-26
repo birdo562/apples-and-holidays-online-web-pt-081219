a721e976562fe2fd3d9cb8d364efa52d5872d34e
Roberto Medina <robby.medina562@gmail.com>
2019-08-26 05:56:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,6 +47,7 @@ def all_winter_holiday_supplies(holiday_hash)
 array = holiday_hash.map do |holiday, item|
   item
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
